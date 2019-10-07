67b811a43ac7f625793db445e727e81c817e8faf
wisdom nwokocha <wise4rmgod@gmail.com>
2019-10-07 14:39:29 +0100

    added navigatin drawer with navigation component

--- app/src/main/java/com/developer/wise4rmgod/landlord/MainActivity.kt
+++ app/src/main/java/com/developer/wise4rmgod/landlord/MainActivity.kt
@@ -2,11 +2,89 @@ package com.developer.wise4rmgod.landlord
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    lateinit var toolbar: Toolbar
+
+    lateinit var drawerLayout: DrawerLayout
+
+    lateinit var navController: NavController
+
+    lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        navigationView = findViewById(R.id.navigationView)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+        navigationView.setNavigationItemSelectedListener(this)
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment)
+            , drawerLayout
+
+        )
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        p0.isChecked = true
+
+        drawerLayout.closeDrawers()
+        val id = p0.getItemId()
+
+        when (id) {
+
+            R.id.first -> navController.navigate(R.id.addtenantFragment)
+
+            R.id.second -> navController.navigate(R.id.deleteTenantFragment)
+
+            R.id.third -> navController.navigate(R.id.addRoomFragment)
+        }
+        return true
+
+    }
+
+
+    // Setting Up One Time Navigation
+    private fun setupNavigation() {
+
+
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
+
